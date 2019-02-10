74c7c4bc591edbb435c6f82451ee0072ab9feb73
vladislav-silitskiy <vladislav.silitskiy@gmail.com>
2019-02-10 18:24:20 +0300

    update

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -7,7 +7,54 @@ namespace PadawansTask3
         public static int Gcd(int a, int b)
         {
             // put your code here
-            throw new NotImplementedException();
+
+            int Gcd = 0;
+            int r = 0;
+
+            int t = 0;
+            if (a < b)
+            {
+                t = a;
+                a = b;
+                b = t;
+            }
+
+            if ((a == 0) && (b == 0))
+            {
+                Gcd = 0;
+            }
+            else if (a == 0)
+            {
+                Gcd = b;
+            }
+            else if (b == 0)
+            {
+                Gcd = a;
+            }
+            else if ((a == b) && (a != 0) && (b != 0))
+            {
+                Gcd = a;
+            }
+            else if (a % b == 0)
+            {
+                Gcd = b;
+            }
+            else if (a > b)
+            {
+                for (r = a % b; a % b != 0;)
+                {
+                    a = b;
+                    b = r;
+                }
+                Gcd = r;
+            }
+            
+            if (Gcd < 0)
+            {
+                Gcd = -Gcd;
+            }
+            return Gcd;
+
         }
     }
 }
